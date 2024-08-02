4a032ee8a5e84eb478e480fd0f8d489c6a91becc
Ryan Conrad <camalot@gmail.com>
2024-08-01 20:40:42 -0500

    move app to subfolder

--- config/routes/token.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const tokenCtrl = require('../../api/controllers/TokenController');
-
-const router = express.Router();
-
-router.route('/api/token').post(tokenCtrl.create);
-router.route('/api/token/:id').delete(tokenCtrl.destroy);
-
-module.exports = router;
