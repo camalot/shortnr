3158b54b84137402b8d60514f07115a8a2430b95
Ryan Conrad <camalot@gmail.com>
2025-04-22 11:05:03 -0500

    better health check?

--- app/routes/health.js
+++ app/routes/health.js
@@ -1,11 +1,26 @@
 const config = require('../config');
 const { Router } = require('express');
+const SettingsMongoClient = require('../api/mongo/Settings')
+
+
 
 const router = Router();
 
-router.get('/health', (req, res) => res.status(200).json({ status: 'ok' }));
-router.get('/healthz', (req, res) => res.status(200).json({ status: 'ok' }));
-router.get('/livez', (req, res) => res.status(200).json({ status: 'ok' }));
-router.get('/readyz', (req, res) => res.status(200).json({ status: 'ok' }));
+async function getHealth(req, res) {
+  try {
+    let settingsClient = new SettingsMongoClient();
+    await settingsClient.connect();
+    await settingsClient.close();
+    res.status(200).json({ status: 'ok' });
+  } catch (err) {
+    res.status(500).json({ status: 'error', message: err.message });
+  }
+}
+
+router.get('/health', getHealth);
+router.get('/healthz', getHealth);
+router.get('/livez', getHealth);
+router.get('/readyz', getHealth);
+
 
 module.exports = router;
